d54773abbf246b6c203e41406185aba3c1d32551
Lehlohonolo.Mokoena <230236839@mycput.ac.za>
2025-07-24 12:07:16 +0200

    the helper class commit

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/util/Helper.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/util/Helper.java
@@ -1,4 +1,20 @@
 package za.ac.student_trade.util;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class Helper {
+
+    public static boolean ValidString(String str) {
+        return str != null && !str.isEmpty();
+    }
+    public static boolean ValidInteger(Integer i) {
+        return i != null && i > 0;
+    }
+    public static boolean validEmail(String email) {
+        EmailValidator validator = EmailValidator.getInstance();
+        return  validator.isValid(email);
+    }
+    public static boolean validLong(long value) {
+        return value > 0;
+    }
 }
