0c033d4a91db6bdb3d7d95689ed3024e50e0f024
Lehlohonolo.Mokoena <hloniyacho@gmail.com>
2025-07-29 10:50:26 +0200

    Student factory commit

---
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/factory/StudentFactory.java
@@ -0,0 +1,21 @@
+package za.ac.student_trade.factory;
+
+import za.ac.student_trade.domain.Residence;
+import za.ac.student_trade.domain.Student;
+import za.ac.student_trade.util.Helper;
+
+public class StudentFactory {
+
+    public static Student createStudent(String firstName, String lastName, String email, String password, Residence residence) {
+
+        String studentId = Helper.generateId();
+        return new Student.Builder()
+                .setStudentId(studentId)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setPassword(password)
+                .setResidence(residence)
+                .build();
+    }
+}
