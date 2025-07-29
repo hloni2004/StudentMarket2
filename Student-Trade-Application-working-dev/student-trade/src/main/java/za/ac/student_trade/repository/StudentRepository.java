812bc08df1495fa6d304c0f8b3f8d31cc132e729
Lehlohonolo.Mokoena <hloniyacho@gmail.com>
2025-07-29 11:12:00 +0200

    Repository and service with tests

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/StudentRepository.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/StudentRepository.java
@@ -1,7 +1,13 @@
 package za.ac.student_trade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.student_trade.domain.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+import java.util.List;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+    List<Student> findByFirstNameAndLastName(String firstName, String lastName);
+    List<Student> findByEmail(String email);
+
 }
--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/service/Impl/StudentServiceImpl.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/service/Impl/StudentServiceImpl.java
@@ -1,4 +1,30 @@
 package za.ac.student_trade.service.Impl;
 
-public class StudentServiceImpl {
+import org.springframework.stereotype.Service;
+import za.ac.student_trade.domain.Student;
+import za.ac.student_trade.repository.StudentRepository;
+import za.ac.student_trade.service.IService;
+@Service
+public class StudentServiceImpl implements IService<Student, String> {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student create(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student read(String id) {
+        return this.studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student update(Student student) {
+        return this.studentRepository.save(student);
+    }
 }
