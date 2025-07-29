d0233ac3151bb49e411e3824f593f1282da22114
nkululeko <nkulufr98@gmail.com>
2025-07-29 17:09:30 +0200

    Factory Address

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/factory/AddressFactory.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/factory/AddressFactory.java
@@ -1,4 +1,28 @@
 package za.ac.student_trade.factory;
 
+import za.ac.student_trade.domain.Address;
+import za.ac.student_trade.util.Helper;
+
+import java.util.Random;
+
 public class AddressFactory {
+    public static Address createAddress(String streetNumber, String streetName, String suburb, String city, String province,
+                                        int postalCode){
+
+        Long addressId = new Random().nextLong();
+
+        if(Helper.isNullOrEmpty(streetNumber) || Helper.isNullOrEmpty(streetName) || Helper.isNullOrEmpty(suburb)
+                || Helper.isNullOrEmpty(city) || Helper.isNullOrEmpty(province) || Helper.postalCodeValid(postalCode)){
+            return new Address.Builder()
+                    .setAddressId(addressId)
+                    .setStreetNumber(streetNumber)
+                    .setStreetName(streetName)
+                    .setSuburb(suburb)
+                    .setCity(city)
+                    .setProvince(province)
+                    .setPostalCode(postalCode)
+                    .build();
+        }
+        return null;
+    }
 }
