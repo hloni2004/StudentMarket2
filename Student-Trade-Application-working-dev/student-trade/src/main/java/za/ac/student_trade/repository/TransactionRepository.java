1647c368ac9f23cbe11d7aa747adf07fb3c995ff
AsandaNdhlela <a.ndhlela@plum.systems>
2025-08-09 15:32:30 +0200

    Transaction <Transaction, String> id fixed

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/TransactionRepository.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package za.ac.student_trade.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.student_trade.domain.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
 }
