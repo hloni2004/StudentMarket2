b181b744ad86a31e1c50bd3fdf0fa21411a0f68c
AsandaNdhlela <a.ndhlela@plum.systems>
2025-08-09 15:31:40 +0200

    Transaction (id) auto generated using UUID

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/factory/TransactionFactory.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/factory/TransactionFactory.java
@@ -7,13 +7,14 @@ import za.ac.student_trade.util.Helper;
 
 import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.UUID;
 
 public class TransactionFactory {
     public static Transaction createTransaction(LocalDateTime transactionDate,String imageOfProduct,String productLabel,String description, String condition, double price, Product productSold, Student buyer) {
 
-
+        String transactionId = UUID.randomUUID().toString();
         return new Transaction.Builder()
-
+                .setTransactionId(transactionId)
                 .setTransactionDate(transactionDate)
                 .setImageOfProduct(imageOfProduct)
                 .setProductLabel(productLabel)
