8c24a7a3f400a0f0d6e11c3cbbd2b81ba9dbe6b3
AsandaNdhlela <a.ndhlela@plum.systems>
2025-08-27 03:23:34 +0200

    Removing unused code

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/controller/ProductController.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/controller/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import za.ac.student_trade.domain.Product;
+import za.ac.student_trade.domain.Student;
 import za.ac.student_trade.service.Impl.ProductServiceImpl;
+import za.ac.student_trade.service.Impl.StudentServiceImpl;
 
 import java.util.List;
 
@@ -18,18 +20,23 @@ import java.util.List;
 public class ProductController {
 
     private ProductServiceImpl productService;
+//    private StudentServiceImpl studentService;
+
 
     @Autowired
     public void setProductService(ProductServiceImpl productService) {
         this.productService = productService;
     }
 
+//    @Autowired
+//    public void setStudentService(StudentServiceImpl studentService) {
+//        this.studentService = studentService;
+//    }
+
     @PostMapping(value = "/create", consumes = "multipart/form-data")
     public ResponseEntity<Product> createProduct(@RequestPart Product product, @RequestPart MultipartFile productImage) {
         try{
-            System.out.println("ProductController.createProduct");
             Product newProduct = productService.addProduct(product, productImage);
-            System.out.println("ProductController");
             return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
         }catch(Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
