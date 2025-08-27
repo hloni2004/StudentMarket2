d40e64f54a35f11587b446cdc8d532f45d0a2f18
AsandaNdhlela <a.ndhlela@plum.systems>
2025-08-27 03:21:46 +0200

    Adding missing @Repository Annotations

--- Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/ResidenceRepository.java
+++ Student-Trade-Application-working-dev/student-trade/src/main/java/za/ac/student_trade/repository/ResidenceRepository.java
@@ -1,7 +1,8 @@
 package za.ac.student_trade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.student_trade.domain.Residence;
-
+@Repository
 public interface ResidenceRepository extends JpaRepository<Residence, Long> {
 }
